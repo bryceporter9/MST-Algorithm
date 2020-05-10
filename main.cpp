a6b6e401e08c7797a41adb6f85897763b298547b
Bryce Porter <49589604+bryceporter9@users.noreply.github.com>
2020-05-09 18:50:24 -0600

    Update main.cpp
    I added notes for what to implement next. I also added a time calculation for the algorithm.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <chrono>
 
+using namespace std::chrono;
 using namespace std;
 
 const int MAX = 1e4 + 5;
@@ -11,13 +13,13 @@ pair <long long, pair<int, int> > p[MAX];
 
 void initialize()
 {
-    for(int i = 0;i < MAX;++i)
+    for (int i = 0; i < MAX; ++i)
         id[i] = i;
 }
 
 int root(int x)
 {
-    while(id[x] != x)
+    while (id[x] != x)
     {
         id[x] = id[id[x]];
         x = id[x];
@@ -36,14 +38,14 @@ long long kruskal(pair<long long, pair<int, int> > p[])
 {
     int x, y;
     long long cost, minimumCost = 0;
-    for(int i = 0;i < edges;++i)
+    for (int i = 0; i < edges; ++i)
     {
         // Selecting edges one by one in increasing order from the beginning
         x = p[i].second.first;
         y = p[i].second.second;
         cost = p[i].first;
         // Check if the selected edge is creating a cycle or not
-        if(root(x) != root(y))
+        if (root(x) != root(y))
         {
             minimumCost += cost;
             union1(x, y);
@@ -59,8 +61,26 @@ int main()
     long long weight, cost, minimumCost;
 
     initialize();
+
+    //open the file
+
+    // Use auto keyword to avoid typing long 
+    // type definitions to get the timepoint 
+    // at this instant use function now() 
+    auto start = high_resolution_clock::now();
+
+    //read in nodes (first line in file)
+
+    //read in x and y (both being the node)
+
+    //read in the weight between x and y
+        //if 1073741824 there is no direct route, so ignore
+        //do not make a pair out of x and y, get next input
+
+    //calculate the number of edges
+
     cin >> nodes >> edges;
-    for(int i = 0;i < edges;++i)
+    for (int i = 0; i < edges; ++i)
     {
         cin >> x >> y >> weight;
         p[i] = make_pair(weight, make_pair(x, y));
@@ -68,6 +88,24 @@ int main()
     // Sort the edges in the ascending order
     sort(p, p + edges);
     minimumCost = kruskal(p);
+
+    // After function call 
+    auto stop = high_resolution_clock::now();
+
+    // Subtract stop and start timepoints and 
+    // cast it to required unit. Predefined units 
+    // are nanoseconds, microseconds, milliseconds, 
+    // seconds, minutes, hours. Use duration_cast() 
+    // function. 
+    auto duration = duration_cast<milliseconds>(stop - start);
+
+    // To get the value of duration use the count() 
+    // member function on the duration object 
+    cout << duration.count() << endl;
+
+    //output the minimum cost
+
     cout << minimumCost << endl;
+
     return 0;
 }
